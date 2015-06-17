b58aba0f84669486a128c3ca74558b2b11ba7f8c
joshua2519 <joshua2519@gmail.com>
2015-06-17 14:26:57 +0800

    add no data exception

--- ETL/stockdog/stockdog_sele_byDate.py
+++ ETL/stockdog/stockdog_sele_byDate.py
@@ -1,6 +1,24 @@
 
 # coding: utf-8
 
+# ###安裝相關軟體
+# 需事先安裝Python 2.7 與pip
+# 
+# selenium webdriver
+# pip install selenium
+# 
+# pyautogui
+# pip install pyautogui
+# 
+# 下載Google chrome driver
+# http://chromedriver.storage.googleapis.com/2.15/chromedriver_win32.zip
+# 解壓縮完後，移動到 C:\python2.7\
+# 
+# ###需要的檔案
+# tradeday.txt
+# topComList.csv
+# 
+
 # In[1]:
 
 import os
@@ -25,7 +43,7 @@ driver= webdriver.Firefox(firefox_profile=fp)
 driver.implicitly_wait(30)
 driver.get("https://www.stockdog.com.tw/")
 driver.find_element_by_name("adgh4r23").clear()
-driver.find_element_by_name("adgh4r23").send_keys("zb101")
+driver.find_element_by_name("adgh4r23").send_keys("iii101")
 driver.find_element_by_name("fsdfy452d").clear()
 driver.find_element_by_name("fsdfy452d").send_keys("123456")
 #輸入驗證碼
@@ -36,7 +54,7 @@ driver.find_element_by_css_selector("#next > button.btn.btn-primary").click()
 
 filepath=r"C:/Program Files (x86)/Google/Chrome/Application/43.0.2357.124/"
 #設定日期區間
-datelist=["2015-05-21","2015-05-20","2015-05-19","2015-05-18","2015-05-15"]
+datelist=["2015-05-21"]
 
 for line in datelist :
     date=line.strip()
@@ -47,11 +65,21 @@ for line in datelist :
             name=row[1]
             outfile=(filepath+name+sid+'_買賣日報_細部籌碼_'+date+'_股狗網.csv').decode('utf-8')
             if(os.path.isfile(outfile)):
-                print outfile+" exist!"
+                print outfile.encode('utf-8')+" exist!"
                 continue
             indexUrl='https://www.stockdog.com.tw/stockdog/index.php?sid='+sid+'+'+name+'&p=1&m=2&date='+date    
             driver.get(indexUrl)
-            export = driver.find_element_by_id('ZeroClipboard_TableToolsMovie_1')
+            try:
+                msg=driver.find_element_by_id('define_table')
+            except:
+                print outfile.encode('utf-8')+" no data!"
+                continue
+            
+            try:
+                export = driver.find_element_by_id('ZeroClipboard_TableToolsMovie_1')
+            except:
+                print "no csv icon!"
+                continue
             time.sleep(3)
             webdriver.ActionChains(driver).move_to_element_with_offset(export,18,18).click().perform()
             time.sleep(3)
@@ -59,7 +87,7 @@ for line in datelist :
             pyautogui.press('tab')
             pyautogui.press('tab')
             pyautogui.press('enter')
-            time.sleep(random.randrange(5,15))
+            time.sleep(random.randrange(10,15))
 
 
 # In[ ]:
