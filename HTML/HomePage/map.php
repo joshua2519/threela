4cd657b66d4439a0ce337223eff25006d7ec56c2
Joshua Chien <joshua2519@gmail.com>
2015-08-10 23:01:04 +0800

    improve counter and start map.php

--- HTML/HomePage/map.php
+++ HTML/HomePage/map.php
@@ -1,9 +1,24 @@
+<?php 
+$host="10.120.30.4";
+$port=3306;
+$socket="";
+$user="threela";
+$password="123456";
+$dbname="threela";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+or die ('Could not connect to the database server' . mysqli_connect_error());
+$query = "select distinct(stockId) FROM stocknearbroker";
+
+
+?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
       <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Free Bootstrap Admin html Template : Master</title>
+    <title>籌碼地緣分析</title>
 	<!-- Bootstrap Styles-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FontAwesome Styles-->
@@ -16,6 +31,43 @@
    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
      <!-- TABLE STYLES-->
     <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+	
+	<style>
+		#map-canvas{
+			width:100%;
+			height:800px;
+		}
+	</style>
+	    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+     <!-- DATA TABLE SCRIPTS -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+        <script>
+            $(document).ready(function () {
+                $('#stockmap').dataTable({
+					"bPaginate": true,
+                    "bLengthChange": false,
+                    "bFilter": false,
+                    "bSort": true,
+                    "bInfo": true,
+                    "bAutoWidth": true,
+				});
+				var pyrmont = new google.maps.LatLng(23.5,120.8);
+				map = new google.maps.Map(document.getElementById('map-canvas'), {
+				center: pyrmont,
+				zoom: 8
+				});
+            });
+			
+			function showMap(stockid){
+				alert(stockid)
+			}
+		</script>
 </head>
 <body>
     <div id="wrapper">
@@ -36,23 +88,26 @@
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
 
-                    <li>
+                     <li>
                         <a href="index.php"><i class="fa fa-dashboard"></i> 大盤資訊</a>
                     </li>
                     
 					<li>
                         <a href="chart.php"><i class="fa fa-bar-chart-o"></i> 個股資訊</a>
                     </li>
+
                     <li>
-                        <a href="counter.php"><i class="fa fa-table"></i> 籌碼面分析</a>
+                        <a href="counter.php"><i class="fa fa-table"></i> 籌碼面推薦</a>
                     </li>
                
                     <li>
-                        <a href="fundamentals.php"><i class="fa fa-edit"></i> 基本面分析</a>
+                        <a  href="fundamentals.php"><i class="fa fa-edit"></i> 基本面推薦</a>
                     </li>
-                    
                     <li>
-                        <a href="map.php" class="active-menu"><i class="fa fa-file"></i> 地圖分析</a>
+                        <a  href="fundpredict.php"><i class="fa fa-edit"></i> 基本面預測</a>
+                    </li>
+                    <li>
+                        <a href="map.php"><i class="fa fa-file"></i> 籌碼地緣分析</a>
                     </li>
 
                 </ul>
@@ -64,41 +119,59 @@
         <div id="page-wrapper" >
             <div id="page-inner">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-4">
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 Map
                             </div>
-                            <div>
-                                地圖位置
+                            <div class="panel-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover" id="stockmap">
+                                        <thead>
+                                            <tr>
+                                                <th>股票代號-名稱</th>
+                                                 <th>功能</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                           		<?php 
+												if ($stmt = $con->prepare($query)) {
+													$stmt->execute();
+													$stmt->bind_result($stockId);
+													while ($stmt->fetch()) {
+														
+														echo "<tr>";														
+														echo "<td>",$stockId,'</td>';
+														echo "<td><input type='button' value='show map' onclick='showMap(",$stockId,")' /></td>";
+														#echo "<td>",$Season,'</td>';
+														#echo "<td>",$individual_ROI,'</td>';
+														#echo "<td>",$y_ROI,'</td>';
+														#echo "<td>",$i_yROI,'</td>';
+														echo "</tr>";                                    	
+													}
+													$stmt->close();
+												}												
+											?> 
+
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
+					<div class="col-xs-8">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<div id="map-canvas"></div>
+							</div>
+						</div>
+					</div>
                 </div>
             </div>
         </div>
            
     
-	</div>
-         <!-- /. PAGE WRAPPER  -->
-     <!-- /. WRAPPER  -->
-    <!-- JS Scripts-->
-    <!-- jQuery Js -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-      <!-- Bootstrap Js -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- Metis Menu Js -->
-    <script src="assets/js/jquery.metisMenu.js"></script>
-     <!-- DATA TABLE SCRIPTS -->
-    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
-    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#dataTables-example').dataTable();
-            });
-		</script>
-         <!-- Custom Js -->
-    <script src="assets/js/custom-scripts.js"></script>
+	</div> 
     
    
 </body>
