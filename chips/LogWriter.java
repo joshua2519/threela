4ed74b69b8aa18984d233df8eb447ad96a462002
wangcb35 <wangcb35@gmail.com>
2015-10-05 22:08:04 +0800

    The brokerBS, bsMenu program use

---
+++ chips/LogWriter.java
@@ -0,0 +1,28 @@
+﻿import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class LogWriter{
+	File logFile;
+	
+	public LogWriter(String filePath) throws IOException{
+		logFile = new File(filePath);
+		
+	}
+	
+	public void writeLog(String msg) throws IOException{		
+		FileWriter logWriter =new FileWriter(logFile,true);
+		Date cal = Calendar.getInstance().getTime();		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String now = df.format(cal);
+		logWriter.write(now+";"+msg+"\n");
+		System.out.println(now+";"+msg);
+		logWriter.flush();
+		logWriter.close();
+	}
+	
+	
+	
+	
+}
